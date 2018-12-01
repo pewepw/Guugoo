d4c203b114fe9b8e3b3ba3c4b2a62e8fa8bab08b
Harry <poripori1030@gmail.com>
2018-12-01 16:01:00 +0800

    Initial Commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Gugo</title>
+    <link rel="stylesheet" typ="text/css" href="assets/css/style.css" />
+  </head>
+  <body>
+  <div class="wrapper indexPage">
+    <div class="mainSection">
+        <div class="logoContainer">
+          <img
+            src="assets/images/logo-guugoo.png"
+            alt=""
+          />
+        </div>
+        <div class="searchContainer">
+          <form action="search.php" method="GET">
+            <input class="searchBox" type="text" name="term" />
+            <input class="searchButton" type="submit" , value="Search" />
+          </form>
+        </div>
+    </div>
+  </div>
+
+  </body>
+</html>
